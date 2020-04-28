f877a910a952b65121adab9aacc7ad5f925b88f1
Nathan Hinish <nathan.hinish@gmail.com>
2020-04-28 13:48:41 -0700

    Building with proper path

---
+++ docs/build/precache-manifest.6df67e0cde6c7dccab1d13adedb167ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97eaafcf6170f23a6dd5b2940dd48959",
+    "url": "/covid-by-state/index.html"
+  },
+  {
+    "revision": "cd7fabb91ce6fc1e48e9",
+    "url": "/covid-by-state/static/css/main.4512c677.chunk.css"
+  },
+  {
+    "revision": "06cc9d7616a5075b8485",
+    "url": "/covid-by-state/static/js/2.1388c768.chunk.js"
+  },
+  {
+    "revision": "2a2d69dad1aed827607410d0302c2346",
+    "url": "/covid-by-state/static/js/2.1388c768.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd7fabb91ce6fc1e48e9",
+    "url": "/covid-by-state/static/js/main.d9924779.chunk.js"
+  },
+  {
+    "revision": "8b4ad578c819e416f5e3",
+    "url": "/covid-by-state/static/js/runtime-main.a6bd1cab.js"
+  }
+]);
