668192107cfbb97274e1d3ac249b6877f2c72fad
nathanhinish <nathanhinish@localhost>
2020-07-14 08:11:16 +0000

    Updating site build

---
+++ docs/precache-manifest.688f32fb3f99144d54c194cb7f8b59ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb5fd9d1e5f6a575fdba547f2bdb0aa7",
+    "url": "/covid-by-state/index.html"
+  },
+  {
+    "revision": "adaa5b22d7ed484a72e5",
+    "url": "/covid-by-state/static/css/main.41ae41f7.chunk.css"
+  },
+  {
+    "revision": "36a1e2be410453912866",
+    "url": "/covid-by-state/static/js/2.ea505cad.chunk.js"
+  },
+  {
+    "revision": "2a2d69dad1aed827607410d0302c2346",
+    "url": "/covid-by-state/static/js/2.ea505cad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adaa5b22d7ed484a72e5",
+    "url": "/covid-by-state/static/js/main.10c3e8e2.chunk.js"
+  },
+  {
+    "revision": "8b4ad578c819e416f5e3",
+    "url": "/covid-by-state/static/js/runtime-main.a6bd1cab.js"
+  }
+]);
