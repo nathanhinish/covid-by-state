1b5b3b73a4eb7be8ef03048663fd0ef5ef64951a
nathanhinish <nathanhinish@localhost>
2020-10-07 16:18:46 +0000

    Updating site build

---
+++ docs/precache-manifest.d36ccac1099c4d24ff3dc1b09ed3d33d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f8b86112d1680f7ba5526646399b8d2",
+    "url": "/covid-by-state/index.html"
+  },
+  {
+    "revision": "d29be57157e58097613d",
+    "url": "/covid-by-state/static/css/main.41ae41f7.chunk.css"
+  },
+  {
+    "revision": "36a1e2be410453912866",
+    "url": "/covid-by-state/static/js/2.ea505cad.chunk.js"
+  },
+  {
+    "revision": "2a2d69dad1aed827607410d0302c2346",
+    "url": "/covid-by-state/static/js/2.ea505cad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d29be57157e58097613d",
+    "url": "/covid-by-state/static/js/main.5a9f8f48.chunk.js"
+  },
+  {
+    "revision": "8b4ad578c819e416f5e3",
+    "url": "/covid-by-state/static/js/runtime-main.a6bd1cab.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid-by-state/precache-manifest.018bfdbdd7bc295bede1587edae1ebcc.js"
+  "/covid-by-state/precache-manifest.d36ccac1099c4d24ff3dc1b09ed3d33d.js"
 );
 
 self.addEventListener('message', (event) => {
