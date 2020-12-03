67b81a0d443b33f679bdf45ced5ed0b4e8070139
nathanhinish <nathanhinish@localhost>
2020-12-03 08:18:36 +0000

    Updating site build

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid-by-state/precache-manifest.54acf00113f800743258755272432d56.js"
+  "/covid-by-state/precache-manifest.da7800c1e56fd90cc685de135a44f2ee.js"
 );
 
 self.addEventListener('message', (event) => {
